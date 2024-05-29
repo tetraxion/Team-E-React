d99b628c17b4a9fba16670e16d2aa9632f7ec2d3
playmakermz <60807663+playmakermz@users.noreply.github.com>
2024-05-29 20:55:36 +0700

    Create header.js

---
+++ Alif-angga/01/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Headd extends React.Component{
+
+    render(){
+        return(
+            <div className="ctn">
+                <h1>Halo saya adalah header</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default Headd
