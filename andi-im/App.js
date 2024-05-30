a9600b8a7386ba3c89b779e4c89059b8f99ed7e1
Andi-IM <andi.irhamm@gmail.com>
2024-05-30 08:16:23 +0700

    create react app

---
+++ andi-im/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor(prop){
+        super(prop)
+        this.state = {count: 0, name: 'budi'} // state === property
+      }
+    
+      handleClick(){
+        this.setState({count: this.state.count + 1}) // state change
+      }
+    
+      render(){
+        return(
+          <div className='rt'>
+            <p>Halo {this.state.count}</p> {/* <======== menampilkan component ====== */}
+            <button onClick={() => {this.handleClick()}}> + </button>
+          </div>
+        )
+      }
+    
+}
+
+export default App
