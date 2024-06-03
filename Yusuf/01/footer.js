91e9c235d25d735f4fa8985217bb16a85c1cee43
Muhammad Yusuf <yusuftkr077@gmail.com>
2024-06-03 21:46:33 +0700

    coba 01

---
+++ Yusuf/01/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Footer extends React.Component{
+
+    render(){
+        return(
+            <div className="ctn">
+                <h1>Halo saya adalah footer</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default Footer
