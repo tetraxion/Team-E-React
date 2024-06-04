16d6b4e9bb10e558755e4bad718d520a8f80e3c9
AlamGarry <60461213+AlamGarry@users.noreply.github.com>
2024-06-04 21:21:46 +0700

    counter App

---
+++ fariq/noun-main/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+import Counter from './counter';
+import Form from './form';
+
+const App = () => {
+  return (
+    <div className='main'>
+        <Counter />
+        <Form />
+
+    </div>
+  );
+};
+
+
+
+
+
+export default App;
